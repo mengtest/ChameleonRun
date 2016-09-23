4788d88981e10d496a8314b4d311ffd42116b1ee
twenty0ne <twenty0ne@qq.com>
2016-09-24 05:28:34 +0800

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,24 +2,25 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	public GameObject m_PlatformPrefab;
-	[HideInInspector] public GameObject m_Platform;
+//	public GameObject m_PlatformPrefab;
+//	[HideInInspector] public GameObject m_Platform;
+	public CameraControl m_CameraControl;
 
 	// Use this for initialization
 	void Start () {
-		// Init platforms
-		m_Platform = Instantiate(m_PlatformPrefab);
-		m_Platform.transform.position = new Vector3(-4f, 1f, 0f);
-		m_Platform.transform.localScale = new Vector3 (2f, 1f, 1f);
-
-		MeshRenderer[] renders = m_Platform.GetComponentsInChildren<MeshRenderer>();
-		for (int i = 0; i < renders.Length; ++i) {
-			renders [i].material.color = new Color (1f, 0f, 0f);
-		}
+//		// Init platforms
+//		m_Platform = Instantiate(m_PlatformPrefab);
+//		m_Platform.transform.position = new Vector3(-4f, 1f, 0f);
+//		m_Platform.transform.localScale = new Vector3 (2f, 1f, 1f);
+//
+//		MeshRenderer[] renders = m_Platform.GetComponentsInChildren<MeshRenderer>();
+//		for (int i = 0; i < renders.Length; ++i) {
+//			renders [i].material.color = new Color (1f, 0f, 0f);
+//		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		print ("GameManager::update");
 	}
 }
